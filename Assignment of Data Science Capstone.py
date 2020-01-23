a7bb586b7fc8ada8261b339ce5765be863d86d79
KhaiHoan95 <58926881+KhaiHoan95@users.noreply.github.com>
2020-01-23 11:27:00 +0700

    Add files via upload

---
+++ Assignment of Data Science Capstone.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# hi, my name is Hoan , this is my assginment for the course Data Sciecne Capstone
+
+
+# In[2]:
+
+
+import numpy as np 
+import pandas as pd 
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
